83c3f94f2fa70a9f77591813f836612f23e013ce
Aaron Phillips <101356609+aaronbasha@users.noreply.github.com>
2022-05-30 08:26:00 -0700

    Add files via upload

---
+++ Button_Clicker/Buttonclicker.js
@@ -0,0 +1,13 @@
+function turnOff(element) {
+    element.innerText = "Logout";
+}
+
+function vanish(element) {
+    element.innerText = "";
+}
+
+
+
+function displayName(elementName) {
+    console.log("elementName");
+}
