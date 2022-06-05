0e97391155d79d351107ed4dfe5b45b36bca59b9
Aaron Phillips <101356609+aaronbasha@users.noreply.github.com>
2022-06-04 21:41:20 -0700

    Add files via upload

---
+++ Home_page_Clone/yellow.js
@@ -0,0 +1,16 @@
+// function turnOff(element) {
+//     element.innerText = "Logout";
+// }
+
+function setAlert(){
+    alert("Your cart is empty")
+}
+
+
+
+
+
+
+
+
+// document.querySelector('.link').addEventListener('click',() =>)
